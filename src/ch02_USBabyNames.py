4e89b8e5195b4539b9d5a8051c5509e9829bde30
Knight.zhou <youngwelle@gmail.com>
2012-12-18 13:20:43 +0800

    CHANGED:applied the function list of chapter02;

--- src/ch02_USBabyNames.py
+++ src/ch02_USBabyNames.py
@@ -9,6 +9,8 @@ logging.basicConfig()
 log = logging.getLogger(None)
 log.setLevel(logging.DEBUG) #set verbosity to show all messages of severity >= DEBUG
 log.info("Starting ch03_USBabyNames!")
+#import matplotlib.pyplot as plot
+#plot(np.arange(10))
 #Load into DataFrame
 names1880 = pd.read_csv('../ch02/names/yob1889.txt',names=['name','sex','births'])
 logging.info("names1880 raw data:")
@@ -82,6 +84,7 @@ logging.info(total_births)
 subset = total_births[['John','Harry','Mary','Marilyn']]
 logging.info('Name trends subset:')
 logging.info(subset)
+#Measuring the increase in naming diversity
 #Plot
 #subset.plot(subplots=True,figsize=(12,10),grid=False,title='Number of births per year')
 #Boy names from 2010:
@@ -94,3 +97,67 @@ logging.info('How many of the most popular names it takes to reach 50%:')
 logging.info(prop_cumsum[:10])
 #Search sorted
 logging.info(prop_cumsum.searchsorted(0.5))
+#In 1900
+df = boys[boys.year == 1990 ]
+in1990 = df.sort_index(by='prop',ascending=False).prop.cumsum()
+in1990_searchsorted = in1990.searchsorted(0.5)+1
+logging.info("in 1900 this number was much smaller:")
+logging.info(in1990_searchsorted)
+#the count for each group
+def get_quantile_count(group,q=0.5):
+    group = group.sort_index(by='prop',ascending=False)
+    return group.prop.cumsum().searchsorted(q)+1
+diversity = top1000.groupby(['year','sex']).apply(get_quantile_count)
+diversity = diversity.unstack('sex')
+logging.info("quantile count:")
+logging.info(diversity.head())
+#diversity.plot(title="Number of popular names in top 50%")
+#The "Last letter " revolution
+# extract last letter from name column
+get_last_letter = lambda x:x[-1]
+last_letters = names.name.map(get_last_letter)
+last_letters.name = 'last_letter'
+table = names.pivot_table('births',rows=last_letters,cols=['sex','year'],aggfunc=sum)
+#select out three representative years spanning the history and print the first few rows:
+subtable = table.reindex(columns=[1910,1960,2010],level='year')
+logging.info("select out three representative years spanning the history and print the first few rows:")
+logging.info(subtable.head())
+#a new table containing proportion of total births for each sex ending in each letter:
+logging.info("a new table containing proportion of total births for each sex ending in each letter:")
+logging.info(subtable.sum())
+#letter prop
+letter_prop = subtable / subtable.sum().astype(float)
+logging.info("letter prop:")
+logging.info(letter_prop)
+#Plot letter prop
+#import matplotlib.pyplot as plt
+#fig, axes = plt.subplots(2, 1, figsize=(10, 8))
+#letter_prop['M'].plot(kind='bar', rot=0, ax=axes[0], title='Male')
+#letter_prop['F'].plot(kind='bar', rot=0, ax=axes[1], title='Female',legend=False)
+#transposing to make each column a time series:
+letter_prop = table/table.sum().astype(float)
+dny_ts = letter_prop.ix[['d','n','y'],'M'].T
+logging.info("transposing to make each column a time series:")
+logging.info(dny_ts.head())
+#a plot of the trends over time again with its plot method
+#dny_ts.plot()
+#Boy names that became girl names (and vice versa)
+all_names = top1000.name.unique()
+mask = np.array(['lesl' in x.lower() for x in all_names])
+lesley_like = all_names[mask]
+logging.info("lesley_like in all names:")
+logging.info(lesley_like)
+#filter down to just those names and sum births grouped by name
+#to see the relative frequencies:
+filtered = top1000[top1000.name.isin(lesley_like)]
+filteredResult = filtered.groupby('name').births.sum()
+logging.info('filter down the lesley like names:')
+logging.info(filteredResult)
+#aggregate by sex and year and normalize within year:
+table = filtered.pivot_table('births',rows='year',cols='sex',aggfunc='sum')
+table = table.div(table.sum(1),axis=0)
+logging.info("aggregate by sex and year and normalize within year:")
+logging.info(table.tail())
+#Lastly,it's now easy to make a plot of the breakdown by sex over time
+#table.plot(style={'M': 'k-', 'F': 'k--'})
+#Conclusions and The Path Ahead
