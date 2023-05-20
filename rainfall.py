c761dec4a77a0439643029b988c80e80d2cf8c83
Schrodi <82058001+simolamine@users.noreply.github.com>
2023-05-20 23:52:56 +0100

    rain sunday number

---
+++ rainfall.py
@@ -0,0 +1,13 @@
+# starting to program to compure average to rainfall
+
+monday = 1
+tuesday = 1.2
+wednesday = 2.5
+thursday = 2.5
+friday = 3
+saturday = 5
+sunday  = 8
+
+ave = (monday + tuesday + wednesday + thursday + friday +saturday +sunday)/7
+
+print("the average rainfall in mm is", ave)
